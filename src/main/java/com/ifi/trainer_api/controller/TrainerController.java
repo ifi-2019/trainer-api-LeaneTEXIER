3b71cfa7b5b93291bd1afc83e128ceeeb6d458ee
Leane <texierl@b11p12.fil.univ-lille1.fr>
2019-11-18 11:03:36 +0100

    [Add] PostMapping to create trainer

--- src/main/java/com/ifi/trainer_api/controller/TrainerController.java
+++ src/main/java/com/ifi/trainer_api/controller/TrainerController.java
@@ -3,10 +3,7 @@ package com.ifi.trainer_api.controller;
 import com.ifi.trainer_api.bo.Trainer;
 import com.ifi.trainer_api.service.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/trainers")
@@ -26,5 +23,10 @@ public class TrainerController {
     @GetMapping(value = "/{name}")
     Trainer getTrainer(@PathVariable String name){
         return this.trainerService.getTrainer(name);
+    }
+
+    @PostMapping("/")
+    Trainer createTrainer(@RequestBody Trainer trainer){
+        return this.trainerService.createTrainer(trainer);
     }
 }
